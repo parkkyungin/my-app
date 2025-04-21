bb2ed9b3ee52f1661498d2a2a51e72d6a383e705
parkkyungin <ruddls1029@gmail.com>
2025-04-21 06:35:19 +0000

    Docker commit

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
 def home():
-return "Hello from Docker!"
-
+    return "Hello from Docker!"
 if __name__ == "__main__":
-app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
